2f6664f2f7a073a6fb5cfa278e7372af15a6f0b9
Lazakralj <113786826+Lazakralj@users.noreply.github.com>
2022-09-19 20:43:58 +0200

    Add files via upload

---
+++ StopWatch/app.js
@@ -0,0 +1,48 @@
+
+  var dugmeStart = document.getElementById("start");
+  var dugmeStop = document.getElementById("stop");
+  var dugmeReset = document.getElementById("reset")
+  var tens = 0;
+  var minuts = 0;
+  var interval;
+  var sekunde = document.getElementById("seconds");
+  var minuti = document.getElementById("hours");
+  dugmeStart.addEventListener("click", function() {
+         interval = setInterval(count, 10)
+  })
+  
+    function count () {
+        tens++;
+        sekunde.textContent = "0" + tens;
+        if (tens > 9) {
+            sekunde.textContent = tens;
+        }
+        if(tens >= 99) {
+            tens = 0;
+            minuti.textContent = "0" + ++minuts;
+        }
+        if(minuts > 9) {
+            minuti.textContent = minuts;
+        } 
+        if (minuts >= 60) {
+            minuts = 0;
+            
+        }
+    }
+
+    dugmeStop.addEventListener("click", function() {
+        clearInterval(interval)
+    })
+ 
+    dugmeReset.addEventListener("click", function() {
+        
+        clearInterval(interval)
+        tens = 0;
+        minuts =0;
+        sekunde.textContent = "00";
+        minuti.textContent = "00";
+        
+    })
+  
+
+
